f51447e4554ba1c70a835bc4d0ea4e973c80be7c
Pallav-Choudhary <pallav.choudhary@42peaks.co>
2026-07-06 23:42:14 +0530

    final changes

--- bytebuzz-frontend/src/config/AxiosHelper.js
+++ bytebuzz-frontend/src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:9090";
+export const baseURL = import.meta.env.VITE_API_URL;;
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
